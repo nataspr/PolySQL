dfd1a3f1d6bf3c7efad8fc3f1515f7d68bc0d520
Lihah <Lihacheva03@yandex.ru>
2024-06-26 00:04:55 +0300

    Наконец-то подключились к серверу

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const pool = require('./dbconfig');
+//создание сервера
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '10mb' })); // Увеличиваем лимит размера тела запроса
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+
+app.post('/register', async (req, res) => {
+    const { login, password, fio } = req.body;
+
+    try {
+        const result = await pool.query(
+            'INSERT INTO users (login, password, fio) VALUES ($1, $2, $3) RETURNING *',
+            [login, password, fio]
+        );
+        res.status(201).json({ message: 'User registered successfully', user: result.rows[0] });
+    } catch (error) {
+        console.error('Error inserting user:', error);
+        res.status(500).json({ message: 'Error registering user' });
+    }
+});
+
+app.listen(port, () => {
+    console.log("Server running on http://localhost:${port}",port);
+});
--- src/index.js
+++ src/index.js
@@ -16,26 +16,6 @@ import NotFound from './views/not-found'
 import Register from './views/register'
 
 const App = () => {
-
-  const [state, setState] = useState(null);
-
-  const callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
-  };
-
-  // получение GET маршрута с сервера Express, который соответствует GET из server.js
-  useEffect(() => {
-    callBackendAPI()
-        .then(res => setState(res.express))
-        .catch(err => console.log(err));
-  }, [])
-
   return (
     <Router>
       <Switch>
