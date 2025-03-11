9528f7be17f3dd6e0f9d37d7605597d7724381f3
Spiridonova Nataliia <spiridonova.nataliia@gmail.com>
2025-03-11 16:15:00 +0300

    Добавлены (возвращены) cookie для id пользователя

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,7 +5,29 @@ const login = async (req, res) => {
     try {
         const result = await authService.login(login, password);
         if (result.success) {
-            res.status(200).json(result);
+            
+            const user = result.user;
+
+            // Очистка всех куков
+            res.clearCookie('user_id', { path: '/' });
+            res.clearCookie('login', { path: '/' });
+            res.clearCookie('fio', { path: '/' });
+            res.clearCookie('role_id', { path: '/' });
+
+            // Установить новые куки, доступные на стороне клиента
+            res.cookie('user_id', user.user_id, { path: '/' });
+            res.cookie('login', user.login, { path: '/' });
+            res.cookie('fio', user.fio, { path: '/' });
+            res.cookie('role_id', user.role_id, { path: '/' });
+
+            // Вывод установленных cookies в консоль
+            console.log('Cookies set:');
+            console.log('user_id:', user.user_id);
+            console.log('login:', user.login);
+            console.log('fio:', user.fio);
+            console.log('role_id:', user.role_id);
+
+            res.status(200).json({ success: true, user });
         } else {
             res.status(401).json(result);
         }
